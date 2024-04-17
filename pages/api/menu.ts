4e980b02f34d3c4429d12e42fdf1879e161e0266
flyzeronew <flyzeronew@gmail.com>
2024-04-17 18:23:13 +0800

    fix:補type

--- pages/api/menu.ts
+++ pages/api/menu.ts
@@ -1,5 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-
+type Data = {
+    name: string;
+};
 export default function handler(  
     req: NextApiRequest,
     res: NextApiResponse<Data>,
